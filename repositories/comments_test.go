ac43cdee13900517b2d4b7455f2cb70ce0ad4dec
orimono <hayaome123@gmail.com>
2026-04-17 00:14:13 +0900

    TestSelectCommentListの追加

--- repositories/comments_test.go
+++ repositories/comments_test.go
@@ -37,7 +37,43 @@ func TestInsertComment(t *testing.T) {
 func TestSelectCommentList(t *testing.T) {
 
 	test := []struct {
+		Articleid int
 		testTitle string
-		expected  models.Comment
-	}{}
+		expected  []models.Comment
+	}{
+		{
+
+			testTitle: "comment1",
+			Articleid: 1,
+			expected: []models.Comment{
+				{CommentID: 1,
+
+					Message: "1st comment yeah"},
+			},
+		}, {
+			testTitle: "comment2",
+			Articleid: 2,
+			expected: []models.Comment{
+				{CommentID: 2,
+					Message: "welcome"},
+			},
+		},
+	}
+
+	for i := 0; i < len(test); i++ {
+		t.Run(test[i].testTitle, func(t *testing.T) {
+			comments, err := repositories.SelectCommentList(testDB, test[i].Articleid)
+			if err != nil {
+				t.Fatal(err)
+			}
+			if num := len(comments); num != len(test[i].expected) {
+				t.Fatal(err)
+			}
+
+			if comments[i].CommentID != test[i].expected[i].CommentID {
+
+			}
+		})
+	}
+
 }
